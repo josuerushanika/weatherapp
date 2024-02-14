e20f94f7f15a82b354f2de7fccac1a1cba727893
josuerushanika <josuerushanika@gmail.com>
2024-02-14 11:18:01 +0200

    Update components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-   <div> Hello </div>
+    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
+      <Navbar />
+    </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,8 @@ export default function Navbar({}: Props) {
       Navbar
       <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto">
         <p className="flex items-center justify-center gap-2 ">
-          <h2 className="text-gray-500 text-3xl"> Weater</h2>
         </p>
+        <h2 className="text-gray-500 text-3xl"> Weater</h2>
       </div>
     </nav>
   );
