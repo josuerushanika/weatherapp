417e13c654ad232a8b5476301dfc228514965918
josuerushanika <josuerushanika@gmail.com>
2024-02-14 17:15:41 +0200

    Update files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,12 +78,16 @@ export default function Home() {
       return data;
     }
   });
-   console.log("data", data)
-  if (isLoading) return 'Loading...'
+  if (isLoading) return <div className="flex items-center min-h-screen justify-center">
+    <p className="animate-bounce">Loading...</p>
+  </div>
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7x1 mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+
+      </main>
     </div> 
   );
 }
