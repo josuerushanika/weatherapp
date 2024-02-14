1ed7ae010c35b1fbcd4748e20ee72d3a2b1595ba
josuerushanika <josuerushanika@gmail.com>
2024-02-14 12:12:39 +0200

    Add sun icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdWbSunny } from "react-icons/md";
 
 type Props = {};
 
@@ -7,10 +8,10 @@ export default function Navbar({}: Props) {
     <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
       <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto">
         <p className="flex items-center justify-center gap-2 ">
+          <h2 className="text-gray-500 text-3xl"> Weater</h2>
+          <MdWbSunny className="text-3x1 mt-1 text-yellow-300"/>
         </p>
-        <h2 className="text-gray-500 text-3xl"> Weater</h2>
       </div>
-      Navbar
     </nav>
   );
 }
