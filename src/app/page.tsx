c7e84363156868b0f177b67681696ad589d650aa
josuerushanika <josuerushanika@gmail.com>
2024-02-14 16:15:25 +0200

    Fixing some bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,82 @@
+'use client'
+
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import { useQuery } from "react-query";
+import axios from "axios";
+
+ //https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=f826324c5e9f06779cb8827c52441ca1&cnt=56
+
+ interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherEntry[];
+  city: CityInfo;
+}
+
+interface WeatherEntry {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: WeatherInfo[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
+interface WeatherInfo {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface CityInfo {
+  id: number;
+  name: string;
+  coord: {
+    lat: number;
+    lon: number;
+  };
+  country: string;
+  population: number;
+  timezone: number;
+  sunrise: number;
+  sunset: number;
+}
+
 
 export default function Home() {
-  //https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=f826324c5e9f06779cb8827c52441ca1&cnt=56
-
-  interface WeatherData {
-    cod: string;
-    message: number;
-    cnt: number;
-    list: WeatherEntry[];
-    city: CityInfo;
-  }
-  
-  interface WeatherEntry {
-    dt: number;
-    main: {
-      temp: number;
-      feels_like: number;
-      temp_min: number;
-      temp_max: number;
-      pressure: number;
-      sea_level: number;
-      grnd_level: number;
-      humidity: number;
-      temp_kf: number;
-    };
-    weather: WeatherInfo[];
-    clouds: {
-      all: number;
-    };
-    wind: {
-      speed: number;
-      deg: number;
-      gust: number;
-    };
-    visibility: number;
-    pop: number;
-    sys: {
-      pod: string;
-    };
-    dt_txt: string;
-  }
-  
-  interface WeatherInfo {
-    id: number;
-    main: string;
-    description: string;
-    icon: string;
-  }
-  
-  interface CityInfo {
-    id: number;
-    name: string;
-    coord: {
-      lat: number;
-      lon: number;
-    };
-    country: string;
-    population: number;
-    timezone: number;
-    sunrise: number;
-    sunset: number;
-  }
-  
+  const { isLoading, error, data } = useQuery<WeatherData>({
+    queryKey: ['repoData'],
+    queryFn: async () => {
+      const {data} = await axios.get("https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=f826324c5e9f06779cb8827c52441ca1&cnt=2");
+      return data;
+    }
+  });
+   console.log("data", data)
+  if (isLoading) return 'Loading...'
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
