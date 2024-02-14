fdab614b19f9d816bdf86e2c64d1341ddb5807d3
josuerushanika <josuerushanika@gmail.com>
2024-02-14 11:56:26 +0200

    Refactor files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,6 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
-    </div>
+    </div> 
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,12 @@ type Props = {};
 export default function Navbar({}: Props) {
   return (
     <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
-      Navbar
       <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto">
         <p className="flex items-center justify-center gap-2 ">
         </p>
         <h2 className="text-gray-500 text-3xl"> Weater</h2>
       </div>
+      Navbar
     </nav>
   );
 }
