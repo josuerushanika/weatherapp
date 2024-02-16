45a9d92be63568d64753e7ef5c2400af5b33d0a1
josuerushanika <josuerushanika@gmail.com>
2024-02-16 14:08:41 +0200

    refactor files

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,4 +1,12 @@
 import { cn } from "@/utils/cn";
 import React from "react";
 
+type Props = {};
 
+export default function Container(props: React.HTMLProps<HTMLDivElement>) {
+  return (
+    <div
+    {...props}
+    className={cn("w-full bg-white border rounded-xl flex py-4 shadow-sm", props.className)} />
+  );
+}
