8b356646ecfa729739fe8b80888637cea0e44916
josuerushanika <josuerushanika@gmail.com>
2024-02-16 13:51:52 +0200

    Refactor code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
@@ -6,6 +6,7 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import { format, parseISO } from "date-fns";
 import Container from "@/components/Container";
+import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
 
 //https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=f826324c5e9f06779cb8827c52441ca1&cnt=56
 
@@ -70,41 +71,70 @@ interface CityInfo {
 
 export default function Home() {
   const { isLoading, error, data } = useQuery<WeatherData>({
-    queryKey: ['repoData'],
+    queryKey: ["repoData"],
     queryFn: async () => {
       const { data } = await axios.get(
         `https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=2`
-
       );
       return data;
-    }
+    },
   });
 
-  const firstData = data?.list[0]
+  const firstData = data?.list[0];
 
-  console.log("data",data)
-  if (isLoading) return <div className="flex items-center min-h-screen justify-center">
-    <p className="animate-bounce">Loading...</p>
-  </div>
+  console.log("data", data);
+  if (isLoading)
+    return (
+      <div className="flex items-center min-h-screen justify-center">
+        <p className="animate-bounce">Loading...</p>
+      </div>
+    );
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
       <main className="px-3 max-w-7x1 mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
-        <section>
-          <div>
-            <h2 className="flex gap-1 text-2xl items-end"> 
+        <section className="space-y-4">
+          <div className="space-y-2">
+            <h2 className="flex gap-1 text-2xl items-end">
               <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEE")}</p>
-              <p className="text-lg">{format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")}</p>
-             </h2>
+              <p className="text-lg">
+                {format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")}
+              </p>
+            </h2>
             <Container className=" gap-10 px-6 items-center">
-                 <div className="flex flex-col px-4">
-                      
-                 </div>
+              <div className="flex flex-col px-4">
+                <span className="text-5xl">
+                  {convertKelvinToCelsius(firstData?.main.temp ?? 296.37)}°
+                </span>
+                <p className="text-xs space-x-1 whitespace-nowrap">
+                  <span>
+                    {" "}
+                    {convertKelvinToCelsius(firstData?.main.temp_min ?? 0)}{" "}
+                  </span>
+                  <span>
+                    {" "}
+                    {convertKelvinToCelsius(
+                      firstData?.main.temp_max ?? 0
+                    )}°↑{" "}
+                  </span>
+                </p>
+              </div>
+              {/*   Time and Weater Icon */}
+
+              <div className="flex gap-10 sm:gap-16 overflow-auto w-full justify-between pr-3">
+                {data?.list.map((d, i) => (
+                  <div
+                    key={i}
+                    className="flex flex-col justify-between gap-2 items-center text-xs font-semibold"
+                  >
+                    <p>{format(parseISO(d.dt_txt), "h:mm a")}</p>
+                  </div>
+                ))}
+              </div>
             </Container>
           </div>
         </section>
-      
       </main>
     </div>
   );
