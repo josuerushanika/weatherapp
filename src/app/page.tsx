2e9746f6abd7b7050b026253b605b0d0adfcade1
josuerushanika <josuerushanika@gmail.com>
2024-02-19 13:25:50 +0200

    updates files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 
-import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import { useQuery } from "react-query";
 import axios from "axios";
 import { format, parseISO } from "date-fns";
 import Container from "@/components/Container";
 import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
+import WeatherIcon from "@/components/WeatherIcon";
 
-//https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=f826324c5e9f06779cb8827c52441ca1&cnt=56
 
 interface WeatherData {
   cod: string;
@@ -128,7 +127,9 @@ export default function Home() {
                     key={i}
                     className="flex flex-col justify-between gap-2 items-center text-xs font-semibold"
                   >
-                    <p>{format(parseISO(d.dt_txt), "h:mm a")}</p>
+                    <p className="whitespace-nowrap">{format(parseISO(d.dt_txt), "h:mm a")}</p>
+                    <WeatherIcon iconName={d.weather[0].icon}/>
+                    <p>{convertKelvinToCelsius(d?.main.temp ?? 0)}°</p>
                   </div>
                 ))}
               </div>
---
+++ src/components/WeatherIcon.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+
+type Props = {};
+
+export default function WeatherIcon(props: React.HTMLProps<HTMLDivElement> & { iconName: string}) {
+  return (
+    <div {...props} className='cn("relative h-20 w-20")'>
+      <Image
+        width={100}
+        height={100}
+        alt="weather-icon"
+        className="absolute h-full w-full"
+        src={`https://openweathermap.org/img/wn/${props.iconName}.png`}
+      />
+    </div>
+  );
+}
