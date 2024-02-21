e603ccadeed193e0efd6441efe30e2ab154f7a5b
josuerushanika <josuerushanika@gmail.com>
2024-02-21 13:15:22 +0200

    Refactor code

--- src/components/WeatherDetails.tsx
+++ src/components/WeatherDetails.tsx
@@ -1,33 +1,63 @@
-import React from "react";
-import { FiDroplet } from "react-icons/fi";
-import { LuEye } from "react-icons/lu";
-import {MdAir} from "react-icons/md";
-import {ImMeter} from "react-icons/im";
+/** @format */
 
-export interface WeatherDetailsProps {
+import React from "react";
+import { LuEye, LuSunrise, LuSunset } from "react-icons/lu";
+import { FiDroplet } from "react-icons/fi";
+import { MdAir } from "react-icons/md";
+import { ImMeter } from "react-icons/im";
+
+export interface WeatherDetailProps {
   visability: string;
   humidity: string;
   windSpeed: string;
   airPressure: string;
-  sunrize: string;
+  sunrise: string;
   sunset: string;
 }
 
-export default function WeatherDetails(props: WeatherDetailsProps) {
+export default function WeatherDetails(props: WeatherDetailProps) {
+  const {
+    visability = "25km",
+    humidity = "61%",
+    windSpeed = "7 km/h",
+    airPressure = "1012 hPa",
+    sunrise = "6.20",
+    sunset = "18:48"
+  } = props;
+
   return (
     <>
-    <SingleWeatherDetail
-      icon={<LuEye />}
-      information="visability"
-      value={props.visability}
-    />
-
-    <SingleWeatherDetail
-    icon={<FiDroplet />}
-    information="humidity"
-    value={props.humidity}
-  />
-  </>
+      <SingleWeatherDetail
+        icon={<LuEye />}
+        information="Visability"
+        value={visability}
+      />
+      <SingleWeatherDetail
+        icon={<FiDroplet />}
+        information="Humidity"
+        value={humidity}
+      />
+      <SingleWeatherDetail
+        icon={<MdAir />}
+        information="Wind speed"
+        value={windSpeed}
+      />
+      <SingleWeatherDetail
+        icon={<ImMeter />}
+        information="Air Pressure"
+        value={airPressure}
+      />
+      <SingleWeatherDetail
+        icon={<LuSunrise />}
+        information="Sunrise"
+        value={sunrise}
+      />
+      <SingleWeatherDetail
+        icon={<LuSunset />}
+        information="Sunset"
+        value={sunset}
+      />
+    </>
   );
 }
 
@@ -36,7 +66,6 @@ export interface SingleWeatherDetailProps {
   icon: React.ReactNode;
   value: string;
 }
-
 
 function SingleWeatherDetail(props: SingleWeatherDetailProps) {
   return (
