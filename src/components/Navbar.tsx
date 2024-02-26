aa82095b06db02fd3f8b7e5c5793f2e572d8dcd4
josuerushanika <josuerushanika@gmail.com>
2024-02-26 11:23:03 +0200

    Update Navbar components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React from "react";
 import { MdWbSunny } from "react-icons/md";
@@ -7,41 +7,40 @@ import { MdOutlineLocationOn } from "react-icons/md";
 
 import SearchBox from "./SearchBox";
 import { useState} from "react";
-
 import axios from "axios";
 
 const API_KEY = process.env.NEXT_PUBLIC_WEATHER_KEY;
 
 type Props = {};
 
-
 export default function Navbar({}: Props) {
-   const [city, setCity] = useState("");
-   const [error, setError] = useState("");
+  const [city, setCity] = useState("");
+  const [error, setError] = useState("");
 
-   const [suggestions, setSuggestions] = useState<string[]>([]);
-   const [showSuggestions, setSuggestions] = useState (false);
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [showSuggestions, setSuggestions] = useState(false);
 
-   async function handleInputChange(value: string) {
+  async function handleInputChange(value: string) {
     setCity(value);
     if (value.length >= 3) {
       try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/find?q=${value}&appid=${API_KEY}`);
-      
-        const suggestions = response.data.list.map((item:any)=> item.name )
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/find?q=${value}&appid=${API_KEY}`
+        );
+
+        const suggestions = response.data.list.map((item: any) => item.name);
         setSuggestions(suggestions);
         setError("");
         setShowSuggestions(true);
-      } catch(error) {
+      } catch (error) {
         setSuggestions([]);
         setShowSuggestions(true);
       }
-    } 
-    else {
+    } else {
       setSuggestions([]);
       setShowSuggestions(true);
     }
-   }
+  }
 
   return (
     <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
@@ -52,27 +51,52 @@ export default function Navbar({}: Props) {
         </p>
         <section className="flex gap-2 items-center">
           <MdMyLocation className="text-2x1 text-gray-400 hover:opacity-80 cursor-pointer" />
-          <MdOutlineLocationOn  className="text-3xl"/>
+          <MdOutlineLocationOn className="text-3xl" />
           <p className="text-slate-900/80 text-sm">India</p>
-          <div>
+          <div className="relative">
             {/*Search Box Components*/}
-            <SearchBox 
-            value={city}
-            // onSubmit={}
-            onChange={(e) => handleInputChange(e.target.value)}
+            <SearchBox
+              value={city}
+              // onSubmit={}
+              onChange={(e) => handleInputChange(e.target.value)}
             />
+            <suggestionBox />
           </div>
-          <suggestionBox/>
         </section>
       </div>
     </nav>
   );
 }
 
-function suggestionBox() {
+function SuggetionBox({
+  showSuggestions,
+  suggestions,
+  handleSuggestionClick,
+  error
+}: {
+  showSuggestions: boolean;
+  suggestions: string[];
+  handleSuggestionClick: (item: string) => void;
+  error: string;
+}) {
   return (
-    <ul className="mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2">
-        <li></li>
-    </ul>
-  )
+    <>
+      {((showSuggestions && suggestions.length > 1) || error) && (
+        <ul className="mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px]  flex flex-col gap-1 py-2 px-2">
+          {error && suggestions.length < 1 && (
+            <li className="text-red-500 p-1 "> {error}</li>
+          )}
+          {suggestions.map((item, i) => (
+            <li
+              key={i}
+              onClick={() => handleSuggestionClick(item)}
+              className="cursor-pointer p-1 rounded   hover:bg-gray-200"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
