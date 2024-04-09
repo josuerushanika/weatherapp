a10de98b975d777d79e0bc8cbddcbd46cac10b4e
josue rushanika <100031798+josuerushanika@users.noreply.github.com>
2024-04-09 11:05:30 +0200

    Update SearchBox.tsx

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -12,13 +12,7 @@ type Props = {
 export default function SearchBox(props: Props) {
     return (
         <form onSubmit={props.onSubmit} className={cn("flex relative items-center justify-center h-10",props.className)}>
-            <input
-                type="text"
-                onChange={props.onChange}
-                value={props.value}
-                placeholder="search location..."
-                className="px-4 py-2 w-[230px] border border-gray-300 rounded-1-md focus:border-blue-500 h-full"
-            />
+         
             <button className="px-4 py-[9px] bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-600 h-full">
                 <IoSearch />
             </button>
