0fcc75040e7d099ea0bc7537acaf6eb80d6cc27f
josue rushanika <100031798+josuerushanika@users.noreply.github.com>
2024-04-18 10:28:03 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -158,24 +158,4 @@ function SuggetionBox({
   handleSuggestionClick: (item: string) => void;
   error: string;
 }) {
-  return (
-    <>
-      {((showSuggestions && suggestions.length > 1) || error) && (
-        <ul className="mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px]  flex flex-col gap-1 py-2 px-2">
-          {error && suggestions.length < 1 && (
-            <li className="text-red-500 p-1 "> {error}</li>
-          )}
-          {suggestions.map((item, i) => (
-            <li
-              key={i}
-              onClick={() => handleSuggestionClick(item)}
-              className="cursor-pointer p-1 rounded   hover:bg-gray-200"
-            >
-              {item}
-            </li>
-          ))}
-        </ul>
-      )}
-    </>
-  );
-}
+
